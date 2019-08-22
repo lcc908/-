3fc22e13bdc14293202470c9252fc60bda4c1c09
lcc908 <593261887@qq.com>
2019-08-22 20:45:02 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+import './assets/reset.css'
+import 'lib-flexible/flexible'
+
+Vue.use(MintUI)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
---
+++ src/page/home/Home.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="header">
+      <mt-header title="人民融媒">
+        <router-link to="/" slot="left">
+          <mt-button class="icon"></mt-button>
+        </router-link>
+        <mt-button icon="more" slot="right"></mt-button>
+      </mt-header>
+    </div>
+</template>
+
+<script>
+  import { Header } from 'mint-ui';
+    export default {
+        name: "Home",
+      components:{
+        Header
+      }
+    }
+</script>
+
+<style scoped>
+  .header{
+    line-height: 40px;
+  }
+  h1 {
+    color: #000;
+  }
+  .mint-header {
+    background-color: #26a2ff;
+  }
+  .icon {
+    background: url('../../assets/images/logo.png');
+    width: 0.42rem;
+    height: 0.42rem;
+    background-size: 100%;
+  }
+  .mint-header-title {
+    color: #000;
+  }
+</style>
